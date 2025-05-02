9e1335d4596ab0bffc525d175b1fca7f701ead79
Safal Karki <sawfal24@gmail.com>
2025-05-02 21:03:40 +0545

    third exercise complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(givenString) {
+    let newString = [];
+    stringLen = givenString.length;
+    for(let i = stringLen; i >= 0; i--){
+        newString[stringLen - i] = givenString.at(i);
+    }
+
+    newString = newString.join("")
+
+    return newString;
 
 };
 

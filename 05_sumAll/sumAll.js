8767ca4a61398d198a46f5a059d73abe862aa4fb
Safal Karki <sawfal24@gmail.com>
2025-05-11 21:01:50 +0545

    completed 5 no

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || !(Number.isInteger(num1)) || !(Number.isInteger(num2)) ||  (Number.isNaN(num2)) || (Number.isNaN(num1))){
+        return "ERROR";
+    }
+    let sum = 0;
+
+    if(num1 > num2){
+        //swap if greater
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+    }
+
+    for(let i = num1; i <= num2; i++){
+        sum = sum + i;
+    }
+
+    return sum;
 
 };
 
